8c01db399e3f15b4d83dd8572c808eec915a0bda
erinmcmurchie21 <ejm21@ic.ac.uk>
2025-04-03 15:39:15 +0100

    Uniform grid TVD schemes

--- interpolateWallValues.py
+++ interpolateWallValues.py
@@ -1,9 +1,3 @@
-''' Define flux limiting function for advection problems
-    This code is a simple implementation of a flux limiter for a one-dimensional advection problem.
-    It uses the Van Leer flux limiter to compute the flux at the cell interfaces.
-    The code also includes a grid class to handle the grid properties and boundary conditions.
-'''
-
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib as mpl
@@ -35,10 +29,12 @@ class Grid(object):
         self.dx = (xmax - xmin)/(nx)
         self.xCentres = xmin + (np.arange(nx+2*nGhost)-nGhost+0.5)*self.dx
         self.xL = xmin + (np.arange(nx+2*nGhost)-nGhost)*self.dx
-        self.xR = xmin + (np.arange(nx+2*nGhost)+1.0)*self.dx
+        self.xR = xmin + (np.arange(nx+2*nGhost)-nGhost+1.0)*self.dx
 
         # storage for the solution
         self.phi = np.zeros(nx+2*nGhost)
+        self.phi_L = np.zeros(nx+2*nGhost)
+        self.phi_R = np.zeros(nx+2*nGhost)
 
 
     def create_array(self):
@@ -51,10 +47,10 @@ class Grid(object):
 
         for n in range(self.nGhost):
             # left boundary
-            self.phi[self.ilo-1-n] = self.phi[self.ihi-n]
+            self.phi[self.ilo-1-n] = 0 #self.phi[self.ihi-n]
 
             # right boundary
-            self.phi[self.ihi+1+n] = self.phi[self.ilo+n]
+            self.phi[self.ihi+1+n] = 0  #self.phi[self.ilo+n]
     
     def check_grid_values(self):
         """ check the grid values """
@@ -67,15 +63,10 @@ class Grid(object):
         print("ihi:", self.ihi)
         print("starting phi values", self.phi)
 
-
-class Simulation(object):
-
-    def __init__(self, grid, u, slope_type):
+class CalculateInterfaceValues(object):
+    def __init__(self, grid, slope_type):
         self.grid = grid
-        self.t = 0.0 # simulation time
-        self.u = u   # the constant advective velocity
-        self.slope_type = slope_type # slope type for the limiter
-
+        self.slope_type = slope_type
 
     def init_cond(self, type="tophat"):
         """ initialize the data """
@@ -94,19 +85,7 @@ class Simulation(object):
             
             self.grid.phi[:] = (1./6.)*(phi_L + 4*phi_c + phi_R)
 
-
-    def timestep(self):
-        """ return the advective timestep """
-        dt = 0.01
-        return dt
-
-
-    def period(self):
-        """ return the period for advection with velocity u """
-        return (self.grid.xmax - self.grid.xmin)/self.u
-
-
-    def interface_values(self, dt):
+    def interface_values(self):
         """ compute the left and right interface states """
 
         # compute the piecewise linear slopes
@@ -130,109 +109,59 @@ class Simulation(object):
         # loop over all the interfaces.  Here, i refers to the left
         # interface of the zone.  Note that there are 1 more interfaces
         # than zones
-        phi_L = g.create_array()
-        phi_R = g.create_array()
+        g.phi_L = g.create_array()
+        g.phi_R = g.create_array()
 
         for i in range(g.ilo, g.ihi+2):
 
             # left state on the current interface comes from zone i-1
-            phi_L[i] = g.phi[i-1] + 0.5*g.dx*(1.0 - self.u*dt/g.dx)*slope[i-1] #
+            g.phi_L[i] = g.phi[i-1] + 0.5*g.dx*slope[i-1] #
             # right state on the current interface comes from zone i
-            phi_R[i] = g.phi[i] - 0.5*g.dx*(1.0 + self.u*dt/g.dx)*slope[i] # 
+            g.phi_R[i] = g.phi[i] + 0.5*g.dx*slope[i] # 
 
-        return phi_L, phi_R
+        return g.phi_L, g.phi_R
+    
 
-
-    def advection(self, phi_L, phi_R):
-        """
-        Riemann problem for advection -- this is simply upwinding,
-        but we return the flux
-        """
-
-        if self.u > 0.0:
-            return self.u*phi_L
-        else:
-            return self.u*phi_R
-
-
-    def update(self, dt, flux):
-        """ conservative update """
-
-        g = self.grid
-
-        phi_new = g.create_array()
-
-        phi_new[g.ilo:g.ihi+1] = g.phi[g.ilo:g.ihi+1] + \
-            dt/g.dx * (flux[g.ilo:g.ihi+1] - flux[g.ilo+1:g.ihi+2])
-
-        return phi_new
-
-
-    def run_simulation(self, num_periods=1):
-        """ evolve the linear advection equation """
-        self.t = 0.0
-        g = self.grid
-
-        tmax = num_periods*self.period()
-
-        # main evolution loop
-        while self.t < tmax:
-
-            # fill the boundary conditions
-            g.fill_BCs()
-
-            # get the timestep
-            dt = self.timestep()
-
-            if self.t + dt > tmax:
-                dt = tmax - self.t
-
-            # get the interface states
-            phi_L, phi_R = self.interface_values(dt)
-
-            # solve the Riemann problem at all interfaces
-            flux = self.advection(phi_L, phi_R)
-
-            # do the conservative update
-            phi_new = self.update(dt, flux)
-
-            g.phi[:] = phi_new[:]
-
-            self.t += dt
-
-
-
-
-
-    #-------------------------------------------------------------------------
-    # compare limiting and no-limiting
+#-----------------------------------------------------------------------------
 
 xmin = 0.0
 xmax = 1.0
-nx = 10
+nx = 50
 nGhost = 2
 
 g = Grid(nx, nGhost, xmin=xmin, xmax=xmax)
 g.check_grid_values()
 
-u = 1.0
-
-s = Simulation(g, u, slope_type="centered")
-s.init_cond("tophat")
-phi_init = s.grid.phi.copy()
-s.run_simulation(num_periods=1)
+calc = CalculateInterfaceValues(g, slope_type="centered")
+calc.init_cond("tophat")
+phi_init = calc.grid.phi.copy()
+phi_L, phi_R = calc.interface_values()
 
 plt.plot(g.xCentres[g.ilo:g.ihi+1], phi_init[g.ilo:g.ihi+1],
              ls=":", label="Exact")
+print(g.xCentres[g.ilo:g.ihi+1])
+print(phi_init[g.ilo:g.ihi+1])
 
-plt.plot(g.xCentres[g.ilo:g.ihi+1], g.phi[g.ilo:g.ihi+1], label="Centered")
+plt.plot(g.xL[g.ilo:g.ihi+2], g.phi_L[g.ilo:g.ihi+2], label="Centered")
+print(g.xL[g.ilo:g.ihi+2])
+print(g.phi_L[g.ilo:g.ihi+2])
 
-s = Simulation(g, u, slope_type="vanleer")
-s.init_cond("tophat")
-s.run_simulation(num_periods=1)
+plt.plot(g.xR[g.ilo-1:g.ihi+1], g.phi_R[g.ilo-1:g.ihi+1], label="Centered")
+print(g.xR[g.ilo-1:g.ihi+1])
+print(g.phi_R[g.ilo-1:g.ihi+1])
 
-plt.plot(g.xCentres[g.ilo:g.ihi+1], g.phi[g.ilo:g.ihi+1],
-             label="Van Leer")
+calc = CalculateInterfaceValues(g, slope_type="vanleer")
+calc.init_cond("tophat")
+phi_L, phi_R = calc.interface_values()
+
+plt.plot(g.xL[g.ilo:g.ihi+2], g.phi_L[g.ilo:g.ihi+2], label="Van Leer")
+print(g.xL[g.ilo:g.ihi+2])
+print(g.phi_L[g.ilo:g.ihi+2])
+
+plt.plot(g.xR[g.ilo-1:g.ihi+1], g.phi_R[g.ilo-1:g.ihi+1], label="Van Leer")
+print(g.xR[g.ilo-1:g.ihi+1])
+print(g.phi_R[g.ilo:g.ihi+1])
+
 
 plt.legend(frameon=False, loc="best")
 
