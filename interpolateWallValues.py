9fc25db57b5ae8ca2f4cea113b285193232d68b0
erinmcmurchie21 <ejm21@ic.ac.uk>
2025-04-08 13:26:05 +0100

    Implementing non-uniform TVD scheme

--- interpolateWallValues.py
+++ interpolateWallValues.py
@@ -25,16 +25,39 @@ class Grid(object):
         self.ilo = nGhost
         self.ihi = nGhost+nx-1
 
-        # physical coords -- cell-centered, left and right edges
-        self.dx = (xmax - xmin)/(nx)
-        self.xCentres = xmin + (np.arange(nx+2*nGhost)-nGhost+0.5)*self.dx
-        self.xL = xmin + (np.arange(nx+2*nGhost)-nGhost)*self.dx
-        self.xR = xmin + (np.arange(nx+2*nGhost)-nGhost+1.0)*self.dx
+         # physical coords -- cell-centered, left and right edges
+        self.dx_wide = 2* (xmax - xmin) / (2*nx - 9)
+        #print("dx_wide = ", self.dx_wide)
+        self.dx_small = self.dx_wide / 4
+        #print(self.dx_small)
+        xGhost_start = xmin - self.dx_small * (np.flip(np.arange(nGhost))+0.5)
+        xFirst = xmin + (np.arange(3)+0.5)*self.dx_small
+        xCentral = xmin + xFirst[-1] + (np.arange(nx-6)+1/8+1/2)*self.dx_wide
+        xEnd = xmin + xCentral[-1] + (1/8+1/2)*self.dx_wide + self.dx_small*(np.arange(3))
+        xGhost_end = xmax + self.dx_small * (np.arange(nGhost)+0.5)
+        self.xCentres = np.concatenate((xGhost_start, xFirst, xCentral, xEnd, xGhost_end))
+        
+        for n in range(1, nGhost +4):
+            xWalls = self.xCentres[:n] - 0.5*self.dx_small
+        for n in range(nGhost + 4, nx-3+nGhost):
+            xWalls = self.xCentres[:n] - 0.5*self.dx_wide
+        for n in range (nx-3, nx+2*nGhost+2):
+            xWalls = self.xCentres[:n] - 0.5*self.dx_small
+
+        xWalls = np.append(xWalls, self.xCentres[nx+2*nGhost-1] + 0.5*self.dx_small)
+        #print("xWalls = ", xWalls)
+
+        self.xL = xWalls[:nx+2*nGhost]
+        self.xR = xWalls[1:nx+2*nGhost+1]
+        self.diffX = self.xR - self.xL
+        #print(self.xCentres[1:nx+2*nGhost+1])
+        #print(self.xCentres[:nx+2*nGhost-1])
+        print(self.diffX)
 
         # storage for the solution
         self.phi = np.zeros(nx+2*nGhost)
-        self.phi_L = np.zeros(nx+2*nGhost)
-        self.phi_R = np.zeros(nx+2*nGhost)
+        self.phi_L = np.zeros(nx+1)
+        self.phi_R = np.zeros(nx+1)
 
 
     def create_array(self):
@@ -47,21 +70,21 @@ class Grid(object):
 
         for n in range(self.nGhost):
             # left boundary
-            self.phi[self.ilo-1-n] = 0 #self.phi[self.ihi-n]
+            self.phi[self.ilo-1-n] = self.phi[self.ihi-n]
 
             # right boundary
-            self.phi[self.ihi+1+n] = 0  #self.phi[self.ilo+n]
+            self.phi[self.ihi+1+n] = self.phi[self.ilo+n]
     
     def check_grid_values(self):
         """ check the grid values """
-        print("Grid values:")
+        #print("Grid values:")
         print("xL:", self.xL)
         print("xR:", self.xR)
         print("xCentres:", self.xCentres)
-        print("dx:", self.dx)
+        print("Differences", self.xR - self.xL)
         print("ilo:", self.ilo)
         print("ihi:", self.ihi)
-        print("starting phi values", self.phi)
+        #print("starting phi values", self.phi)
 
 class CalculateInterfaceValues(object):
     def __init__(self, grid, slope_type):
@@ -90,77 +113,85 @@ class CalculateInterfaceValues(object):
 
         # compute the piecewise linear slopes
         g = self.grid
-        slope = g.create_array()
+        flux_limiter = g.create_array()
+        #R = g.create_array()
 
         if self.slope_type == "centered":
 
             for i in range(g.ilo-1, g.ihi+2):
-                slope[i] = 0.5*(g.phi[i+1] - g.phi[i-1])/g.dx
+                #modified[i] = 0.5*(g.phi[i+1] + g.phi[i])
+                pass
 
         elif self.slope_type == "vanleer":
-            for i in range(g.ilo-1, g.ihi+2):
+            for i in range(g.ilo-2, g.ihi+1):
                 # van Leer limiter
                 # returns the " limited slowp = van leer function * slope"
                 epsilon = 1.0e-10
-                r = ((g.phi[i+1] - g.phi[i])+ epsilon)/((g.phi[i]-g.phi[i-1])+ epsilon)
-                slope [i] = (r + abs(r))/(1.0 + abs(r)) * (g.phi[i]-g.phi[i-1])/g.dx
-
-
+                R_l = (g.diffX[i] + g.diffX[i-1]) / g.diffX[i-1]
+                r_l = ((g.phi[i-1] - g.phi[i-2]) + epsilon)/((g.phi[i]-g.phi[i-1])+ epsilon)*(g.diffX[i]+g.diffX[i-1])/(g.diffX[i-1] + g.diffX[i-2])
+                modified_van_leer = (0.5 * R_l * r_l + 0.5 * R_l * abs(r_l))/(R_l + r_l - 1 )
+                flux_limiter[i] = modified_van_leer / R_l
+                print("flux_limiter = ", flux_limiter[i])
         # loop over all the interfaces.  Here, i refers to the left
         # interface of the zone.  Note that there are 1 more interfaces
         # than zones
         g.phi_L = g.create_array()
         g.phi_R = g.create_array()
 
-        for i in range(g.ilo, g.ihi+2):
+        for i in range(g.ilo, g.ihi+1):
 
             # left state on the current interface comes from zone i-1
-            g.phi_L[i] = g.phi[i-1] + 0.5*g.dx*slope[i-1] #
+            g.phi_L[i] = g.phi[i-1] + flux_limiter[i] * (g.phi[i]-g.phi[i-1]) #
             # right state on the current interface comes from zone i
-            g.phi_R[i] = g.phi[i] + 0.5*g.dx*slope[i] # 
+            g.phi_R[i] = g.phi[i] + flux_limiter[i+1] * (g.phi[i+1]-g.phi[i]) # 
+
+
+        print("phi_L = ", g.phi_L)
+        print("xL = ", g.xL)
+        print("phi_R = ", g.phi_R)
+        print("xR = ", g.xR)
 
         return g.phi_L, g.phi_R
-    
-
+        
 #-----------------------------------------------------------------------------
 
 xmin = 0.0
 xmax = 1.0
-nx = 50
+nx = 20
 nGhost = 2
 
 g = Grid(nx, nGhost, xmin=xmin, xmax=xmax)
-g.check_grid_values()
+#g.check_grid_values()
 
-calc = CalculateInterfaceValues(g, slope_type="centered")
+calc = CalculateInterfaceValues(g, slope_type="vanleer")
 calc.init_cond("tophat")
 phi_init = calc.grid.phi.copy()
 phi_L, phi_R = calc.interface_values()
 
 plt.plot(g.xCentres[g.ilo:g.ihi+1], phi_init[g.ilo:g.ihi+1],
              ls=":", label="Exact")
-print(g.xCentres[g.ilo:g.ihi+1])
-print(phi_init[g.ilo:g.ihi+1])
+#print(g.xCentres[g.ilo:g.ihi+1])
+#print(phi_init[g.ilo:g.ihi+1])
 
-plt.plot(g.xL[g.ilo:g.ihi+2], g.phi_L[g.ilo:g.ihi+2], label="Centered")
-print(g.xL[g.ilo:g.ihi+2])
-print(g.phi_L[g.ilo:g.ihi+2])
+#plt.plot(g.xL[g.ilo:g.ihi+2], g.phi_L[g.ilo:g.ihi+2], label="Centered")
+#print(g.xL[g.ilo:g.ihi+2])
+#print(g.phi_L[g.ilo:g.ihi+2])
 
-plt.plot(g.xR[g.ilo-1:g.ihi+1], g.phi_R[g.ilo-1:g.ihi+1], label="Centered")
-print(g.xR[g.ilo-1:g.ihi+1])
-print(g.phi_R[g.ilo-1:g.ihi+1])
+#plt.plot(g.xR[g.ilo-1:g.ihi+1], g.phi_R[g.ilo-1:g.ihi+1], label="Centered")
+#print(g.xR[g.ilo-1:g.ihi+1])
+#print(g.phi_R[g.ilo-1:g.ihi+1])
 
 calc = CalculateInterfaceValues(g, slope_type="vanleer")
 calc.init_cond("tophat")
 phi_L, phi_R = calc.interface_values()
 
-plt.plot(g.xL[g.ilo:g.ihi+2], g.phi_L[g.ilo:g.ihi+2], label="Van Leer")
-print(g.xL[g.ilo:g.ihi+2])
-print(g.phi_L[g.ilo:g.ihi+2])
+plt.plot(g.xL[g.ilo:g.ihi+1], g.phi_L[g.ilo:g.ihi+1], label="Van Leer L")
+#print(g.xL[g.ilo:g.ihi+1])
+#print(g.phi_L[g.ilo:g.ihi+1])
 
-plt.plot(g.xR[g.ilo-1:g.ihi+1], g.phi_R[g.ilo-1:g.ihi+1], label="Van Leer")
-print(g.xR[g.ilo-1:g.ihi+1])
-print(g.phi_R[g.ilo:g.ihi+1])
+plt.plot(g.xR[g.ilo-1:g.ihi], g.phi_R[g.ilo-1:g.ihi], label="Van Leer R")
+#print(g.xR[g.ilo:g.ihi+1])
+#print(g.phi_R[g.ilo:g.ihi+1])
 
 
 plt.legend(frameon=False, loc="best")
