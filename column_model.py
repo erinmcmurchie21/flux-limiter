da83e02ab2b92c34f494c2a35ec12f6722e308fd
erinmcmurchie21 <erin.mcmurchie21@imperial.ac.uk>
2025-06-12 16:24:36 +0100

    working PVSA isothermal model!!!!

--- column_model.py
+++ column_model.py
@@ -28,17 +28,17 @@ def quadratic_extrapolation_derivative(x0, y0, x1, y1, x2, y2):
     return y2
 
 def adsorption_isotherm_1(pressure, mole_fraction):
-    K_1 = 6.077 # adsorption equilibrium constant
+    K_1 = 6.077 / 10**5 # adsorption equilibrium constant
     R = 8.314 # J/(mol*K)
     T = 308.15 # K
-    equilibrium_loading = - K_1 * pressure * mole_fraction/ (R*T)
+    equilibrium_loading = K_1 * pressure * mole_fraction/ (R*T)
     return equilibrium_loading
 
 def adsorption_isotherm_2(pressure, mole_fraction):
-    K_2 = 6.077 # adsorption equilibrium constant
+    K_2 = 0 / 10**5 # adsorption equilibrium constant
     R = 8.314 # J/(mol*K)
     T = 308.15 # K
-    equilibrium_loading = - K_2 * pressure * mole_fraction/ (R*T)
+    equilibrium_loading = K_2 * pressure * mole_fraction/ (R*T)
     return equilibrium_loading
 
 class AdsorptionColumn(object):
@@ -70,6 +70,7 @@ class AdsorptionColumn(object):
         self.R = float(bed_properties['R'])
         self.T_column = float(bed_properties['T_column'])
         self.rho_b = float(bed_properties['rho_b'])
+        self.MW = 1/10**3 * np.array([28.0134, 32.01])  # Molecular weights of components in kg/mol, e.g. nitrogen and oxygen
 
         """Import boundary conditions for inlet and outlet"""
         self.inlet_type = inlet_values["inlet_type"]  # e.g. "massflow", "pressure", "closed"
@@ -96,20 +97,23 @@ class AdsorptionColumn(object):
         # Implement inlet boundary conditions
 
         P, y, n1, n2, F = self.data_prep(results_vector)
+        P = np.array(P)
     
         if self.inlet_type == "mass_flow":
             # Mass flow inlet boundary condition
+            
             inlet_values = self.inlet_values
+            y_inlet_ = float(inlet_values["y_feed_value"])
              #Convert from mass flowrate to volumetric flowrate
             vol_flow_inlet_ = float(inlet_values["feed_mass_flow"]) / float(inlet_values["rho_feed"])
+            #avg_density_inlet_ = inlet_values["rho_feed"] * ( self.MW[0] * y_inlet_ + self.MW[1]*(1-y_inlet_))
             #find velocity
-            v_inlet_ = vol_flow_inlet_ * self.bed_voidage / self.column_area
+            v_inlet_ = vol_flow_inlet_  / (self.column_area)
             #use velocity to find dPdZ from ergun equation
             dPdz_inlet_ = 1.75 * (1- self.bed_voidage) * float(inlet_values["rho_feed"]) * v_inlet_**2 / (self.bed_voidage**3 * self.particle_diameter) + 150 * inlet_values["mu"] * (1-self.bed_voidage)**2 *v_inlet_ / (self.bed_voidage**3 * self.particle_diameter**2)
             #use dPdZ to find P_inlet_
             P_inlet_ = P[0] - dPdz_inlet_ * (self.xWalls[int(self.nGhost)] - self.xCentres[int(self.nGhost)])
-            y_inlet_ = float(inlet_values["y_feed_value"])
-            
+           
 
         elif self.inlet_type == "closed":
             # from lagrangian derivative function
@@ -128,15 +132,20 @@ class AdsorptionColumn(object):
 
         if self.outlet_type == "pressure":
             # Mass flow inlet boundary condition
-            P_outlet_ = outlet_values["outlet_pressure"]
-            dPdz_outlet_ = (P_outlet_ - P[-1]) / (self.xWalls[-1] - self.xCentres[-1])
-            #### how do I calculate the desnity in the ergun equation at the outlet??
-            a = 1.75 * (1- self.bed_voidage) * outlet_values["outlet_density"] / (self.bed_voidage**3 * self.particle_diameter)
-            b = 150 * outlet_values["mu"] * (1-self.bed_voidage)**2 / (self.bed_voidage**3 * self.particle_diameter**2)
-            c = dPdz_outlet_
-            v_outlet_ = (-b + np.sqrt(b**2 - 4*a*c)) / (2*a)
             y_outlet_ = quadratic_extrapolation(self.xCentres[-(self.nGhost+1)], y[-1], self.xCentres[-(self.nGhost+2)], y[-2],
                                                self.xCentres[-(self.nGhost+3)], y[-3], self.xWalls[-(self.nGhost+1)])
+            
+            P_outlet_ = outlet_values["outlet_pressure"]
+            rho_g_outlet_ = P_outlet_ / (self.R * self.T_column)  # density at cell walls
+            avg_density_outlet_ = rho_g_outlet_ * (self.MW[0] * y_outlet_ + self.MW[1]*(1-y_outlet_))
+            dPdz_outlet_ = (P_outlet_ - P[-1]) / (self.xWalls[-1] - self.xCentres[-1])
+            #### how do I calculate the desnity in the ergun equation at the outlet??
+            a = 1.75 * (1- self.bed_voidage) * avg_density_outlet_ / (self.bed_voidage**3 * self.particle_diameter)
+            b = 150 * outlet_values["mu"] * (1-self.bed_voidage)**2 / (self.bed_voidage**3 * self.particle_diameter**2)
+            c = np.abs(dPdz_outlet_)
+            v_outlet_ = - np.sign(dPdz_outlet_) * ((-b + np.sqrt(b**2 + 4*a*c)) / (2*a))
+            
+            
 
         elif self.outlet_type == "massflow":
             # Mass flow inlet boundary condition
@@ -165,8 +174,8 @@ class AdsorptionColumn(object):
 
 
         # Left ghost cell, i = -1, using values at i = 1/2, 1, 2
-        P_ghost_start = P[0] - (self.xCentres[0] - self.xCentres[1]) * (P[0] - P_inlet_) / (self.xCentres[self.nGhost] - self.xWalls[self.nGhost])
-        P_ghost_end = P[-1] + (self.xCentres[-1] - self.xCentres[-self.nGhost-1]) * (P[-1] - P_outlet_) / (self.xCentres[-(self.nGhost+1)] - self.xWalls[-(self.nGhost+1)]) 
+        P_ghost_start = P[0] + (self.xCentres[0] - self.xCentres[1]) * (P[0] - P_inlet_) / (self.xCentres[self.nGhost] - self.xWalls[self.nGhost])
+        P_ghost_end = P[-1] - (self.xCentres[-1] - self.xCentres[-self.nGhost-1]) * (P[-1] - P_outlet_) / (self.xCentres[-(self.nGhost+1)] - self.xWalls[-(self.nGhost+1)]) 
         P_centres_and_ghost_cells = np.concatenate((np.array([P_ghost_start]), P, np.array([P_ghost_end])))
         
         # Left ghost cell, i = -1, using values at i = 1/2, 1, 2 # if there is an issue, use linear extrapolation
@@ -186,24 +195,7 @@ class AdsorptionColumn(object):
         Nx = int(self.num_cells)
         epsilon = 1.0e-10
         y_centres_and_ghost_cells, P_centres_and_ghost_cells, P_inlet_, y_inlet_, v_inlet_, dPdz_inlet_, P_outlet_, y_outlet_, v_outlet_, dPdz_outlet_ = self.ghost_cell_calcs(results_vector)
-        #calculate dP/dz at internal cell walls by linear interpolation
-        dPdz_walls = np.array((P_centres_and_ghost_cells[1:Nx+2]- P_centres_and_ghost_cells[0:Nx+1]) / (self.xCentres[1:Nx+2] - self.xCentres[0:Nx+1]))
-        dPdz_walls[0] = dPdz_inlet_  # set inlet pressure gradient
-        dPdz_walls[-1] = dPdz_outlet_
-
-        #calculate velocity vector at cell walls, from Ergun equation
-        a = 1.75 * (1- self.bed_voidage) * self.inlet_values["rho_feed"] / (self.bed_voidage**3 * self.particle_diameter)
-        b = 150 * self.mu * (1-self.bed_voidage)**2 / (self.bed_voidage**3 * self.particle_diameter**2)
-        c = -np.abs(dPdz_walls[:])
-        v_walls = np.array((-b + np.sqrt(b**2 - 4*a*c)) / (2*a)) # should have N + 1 values 
-        v_walls = np.multiply(-np.sign(dPdz_walls),v_walls)  # make sure velocity is in the correct direction
-        v_walls[0] = v_inlet_  # set inlet velocity
-        v_walls[-1] = v_outlet_  # set outlet velocity
-
-        #calculate P at cell walls by interpolation
-        P_walls = P_centres_and_ghost_cells[0:Nx+1] + dPdz_walls * (self.deltaZ[0:Nx+1]/2)
-        P_walls[0] = P_inlet_  # set inlet pressure
-        P_walls[-1] = P_outlet_  # set outlet pressure
+        
 
         #y vector at cell walls, from van leer flux limiter function
         R_r = (self.deltaZ[2:Nx+2] + self.deltaZ[1:Nx+1]) / self.deltaZ[1:Nx+1]
@@ -214,6 +206,31 @@ class AdsorptionColumn(object):
         y_walls[-1] = y_outlet_
         y_walls = np.concatenate((np.array([y_inlet_]), y_walls))
 
+        #calculate dP/dz at internal cell walls by linear interpolation
+        dPdz_walls = np.array((P_centres_and_ghost_cells[1:Nx+2]- P_centres_and_ghost_cells[0:Nx+1]) / (self.xCentres[1:Nx+2] - self.xCentres[0:Nx+1]))
+        dPdz_walls[0] = dPdz_inlet_  # set inlet pressure gradient
+        dPdz_walls[-1] = dPdz_outlet_
+
+        #calculate P at cell walls by interpolation
+        P_walls = np.array(P_centres_and_ghost_cells[0:Nx+1] + dPdz_walls * (self.deltaZ[0:Nx+1]/2))
+        P_walls[0] = P_inlet_  # set inlet pressure
+        P_walls[-1] = P_outlet_  # set outlet pressure
+
+        #calculate velocity vector at cell walls, from Ergun equation
+        rho_g_walls = P_walls / (self.R * self.T_column)  # density at cell walls
+        avg_density_walls = rho_g_walls * (self.MW[0] * y_walls + self.MW[1]*(1-y_walls))
+        a = 1.75 * (1- self.bed_voidage) * avg_density_walls / (self.bed_voidage**3 * self.particle_diameter)
+        b = 150 * self.mu * (1-self.bed_voidage)**2 / (self.bed_voidage**3 * self.particle_diameter**2)
+        c = np.abs(dPdz_walls[:])
+        dominant = b**2+4*a*c
+        
+        if np.any(dominant < 0):
+            raise ValueError("Negative value under square root in velocity calculation. Check your inputs and boundary conditions.")
+        v_walls = np.array((-b + np.sqrt(dominant)) / (2*a)) # should have N + 1 values 
+        v_walls = np.multiply(-np.sign(dPdz_walls),v_walls)  # make sure velocity is in the correct direction
+        v_walls[0] = v_inlet_  # set inlet velocity
+        v_walls[-1] = v_outlet_  # set outlet velocity
+
         return P_walls, y_walls, v_walls
 
 
@@ -222,7 +239,6 @@ class AdsorptionColumn(object):
         # using wall values calculated in previous step
 
         Nx = int(self.num_cells)
-        N_walls = Nx + 1  # number of wall values
 
         P_walls, y_walls, v_walls = self.calculate_wall_values(results_vector)
         P, y, n1, n2, F = self.data_prep(results_vector)
@@ -238,11 +254,11 @@ class AdsorptionColumn(object):
         """ q* and q need to be in concentration units i.e. mol / m3 ----- double check for future"""
         #pressure differential at cell centres
         
-        dPdt = 1/ self.deltaZ[1:-1] * (v_walls[1:]*P_walls[1:] - v_walls[:Nx]*P_walls[:Nx]) - self.rho_b*self.R*self.T_column/self.bed_voidage * (dn1dt[:] + dn2dt[:]) # dP/dz at cell centres
-
+        dPdt = -(1/ self.deltaZ[1:-1]) * (v_walls[1:]*P_walls[1:] - v_walls[:Nx]*P_walls[:Nx]) - self.rho_b*self.R*self.T_column/self.bed_voidage * (dn1dt[:] + dn2dt[:]) # dP/dz at cell centres
+        #dPdt =  - self.rho_b*self.R*self.T_column/self.bed_voidage * (dn1dt[:] + dn2dt[:])  # Initialize dPdt with zeros
         #mole fraction differential at cell centres
-        dydt = 1/ (self.deltaZ[1:-1] * P[:]) * (v_walls[1:]*P_walls[1:]*y_walls[1:] - v_walls[:Nx]*P_walls[:Nx]*(y_walls[:Nx])) - self.rho_b*self.R*self.T_column/self.bed_voidage * dn1dt[:] - y[:]/P[:] * dPdt[:]
-
+        dydt = -(1/ (self.deltaZ[1:-1] * P[:])) * (v_walls[1:]*P_walls[1:]*y_walls[1:] - v_walls[:Nx]*P_walls[:Nx]*y_walls[:Nx]) - self.rho_b*self.R*self.T_column/(self.bed_voidage*P[:]) * dn1dt[:] - y[:]/P[:] * dPdt[:]
+        
 
         # these are my boundary values!!!!
         dF1dt = self.bed_voidage * self.column_area / (self.R * self.T_column) * v_walls[0]*P_walls[0] * y_walls[0]
--- run_adsorption_model.py
+++ run_adsorption_model.py
@@ -3,14 +3,15 @@ import numpy as np
 from column_model import AdsorptionColumn, adsorption_isotherm_1, adsorption_isotherm_2
 from scipy.integrate import solve_ivp
 from non_uniform_grid import create_non_uniform_grid
+import matplotlib.pyplot as plt
 
 bed_properties = {
     "bed_voidage" : 0.4,  # Example value for bed voidage
-    "particle_diameter" : 0.0003,  # Example value for particle diameter in meters
+    "particle_diameter" : 0.0002,  # Example value for particle diameter in meters
     "bed_diameter" : 0.1,  # Example value for bed diameter in meters
     "column_area" : 0.1**2 * np.pi / 4 , # Cross-sectional area of the column
     "R" : 8.314,  # Universal gas constant in J/(mol*K)
-    "T_column" : 308,  # Column temperature in Kelvin
+    "T_column" : 298,  # Column temperature in Kelvin
     "rho_b" : 1000, # Example value for bed density in kg/m^3
     "bed_length" : 1.8,  # Example value for bed length in meters
 }
@@ -19,12 +20,13 @@ column_grid = create_non_uniform_grid()
 
 inlet_values = {
     "inlet_type" : "mass_flow", 
-    "feed_mass_flow" : 0.001,  # Example value for feed mass flow in kg/s
-    "feed_temperature" : 308,  # Example value for feed temperature in Kelvin
+    "velocity" : 1.0,  # Example value for superficial velocity in m/s
+    "feed_mass_flow" : (0.1*bed_properties["column_area"] * 1.2),  # Example value for feed mass flow in kg/s
+    "feed_temperature" : 298,  # Example value for feed temperature in Kelvin
     "feed_pressure" : 101325,  # Example value for feed pressure in Pa
     "rho_feed" : 1.2,  # Example value for feed density in kg/m^3
     "mu" : 1.8e-5,  # Example value for feed viscosity in Pa.s
-    "y_feed_value" : 1.0,  # Example value for feed mole fraction
+    "y_feed_value" : 0.05,  # Example value for feed mole fraction
 }
 
 outlet_values = {
@@ -35,8 +37,8 @@ outlet_values = {
     }
 P = np.ones(column_grid["num_cells"]) * 101325  # Example pressure vector in Pa
 y = np.ones(column_grid["num_cells"]) * 1e-6
-n1 = adsorption_isotherm_1(P, y) # Example concentration vector in mol/m^3
-n2 = adsorption_isotherm_2(P, 1-y) # Example concentration vector in mol/m^3
+n1 = np.ones(column_grid["num_cells"]) * adsorption_isotherm_1(P, y) # Example concentration vector in mol/m^3
+n2 = np.ones(column_grid["num_cells"]) * adsorption_isotherm_2(P, 1-y) # Example concentration vector in mol/m^3
 F = np.ones(4) * 0
 initial_conditions = np.concatenate([P,y,n1,n2,F])
 # Create an instance of the AdsorptionColumn class
@@ -46,7 +48,7 @@ column_model = AdsorptionColumn(column_grid, bed_properties, inlet_values, outle
 t_span = [0, 10]  # Time span for the ODE solver
 rtol = 1e-6
 atol_P = 1e-2 * np.ones(len(P))
-atol_y = 1e-6 * np.ones(len(y))
+atol_y = 1e-9 * np.ones(len(y))
 atol_n1 = 1e-3 * np.ones(len(n1))
 atol_n2 = 1e-3 * np.ones(len(n2))
 atol_F = 1e-4 * np.ones(len(F))
@@ -55,3 +57,56 @@ output_matrix = solve_ivp(column_model.ODE_calculations, t_span, initial_conditi
 
     
 print(output_matrix)
+
+P_result = output_matrix.y[0:column_grid["num_cells"]]
+y_result = output_matrix.y[column_grid["num_cells"]:2*column_grid["num_cells"]]
+n1_result = output_matrix.y[2*column_grid["num_cells"]:3*column_grid["num_cells"]]
+n2_result = output_matrix.y[3*column_grid["num_cells"]:4*column_grid["num_cells"]]
+F_result = output_matrix.y[4*column_grid["num_cells"]:]
+
+P_plot = [P_result[0], P_result[14], P_result[29]]
+y_plot = [y_result[0], y_result[14], y_result[29]]
+n1_plot = [n1_result[0], n1_result[14], n1_result[29]]
+n2_plot = [n2_result[0], n2_result[14], n2_result[29]]
+print("Time points =", len(output_matrix.t))
+
+# Create the plot
+plt.figure(figsize=(6, 4))
+data1 = P_result[0]
+data2 = P_result[14]
+data3 = P_result[29]
+
+# Plot each dataset
+plt.plot(output_matrix.t, data1, label='First node', linewidth=2, marker='o', markersize=3)
+plt.plot(output_matrix.t, data2, label='Central node', linewidth=2, marker='s', markersize=3)
+plt.plot(output_matrix.t, data3, label='Final node', linewidth=2, marker='^', markersize=3)
+
+# Customize the plot
+plt.title('Pressure against time', fontsize=16, fontweight='bold')
+plt.xlabel('Time', fontsize=12)
+plt.ylabel('Pressure', fontsize=12)
+plt.legend(fontsize=11)
+plt.grid(True, alpha=0.3)
+
+# Show the plot
+plt.show()
+
+# Create the plot
+plt.figure(figsize=(6, 4))
+data1 = y_result[0]
+data2 = y_result[14]
+data3 = y_result[29]
+
+# Plot each dataset
+plt.plot(output_matrix.t, data1, label='First node', linewidth=2, marker='o', markersize=3)
+plt.plot(output_matrix.t, data2, label='Central node', linewidth=2, marker='s', markersize=3)
+plt.plot(output_matrix.t, data3, label='Final node', linewidth=2, marker='^', markersize=3)
+
+# Customize the plot
+plt.xlabel('Time', fontsize=12)
+plt.ylabel('Gas phase mole fraction', fontsize=12)
+plt.legend(fontsize=11)
+plt.grid(True, alpha=0.3)
+
+# Show the plot
+plt.show()
